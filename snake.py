9b3cba0715afa218d7cf46d5007984dfa11b4f44
werea1 <34074695+werea1@users.noreply.github.com>
2017-11-30 15:22:02 -0500

    Update snake.py
    add down and right directions

--- snake.py
+++ snake.py
@@ -30,7 +30,7 @@ DIRECTION_DOWN = (0, 1)
 # Background color of the snake grid.
 COLOR_BACKGROUND = (255, 255, 255)  # rgb color for white
 # This is the color of the snake's head. 
-COLOR_SNAKE_HEAD = (159, 55, 151)      # rgb color for red
+COLOR_SNAKE_HEAD = (159, 55, 151)      # rgb color for purple
 # This is the color of the rest of the snake.
 COLOR_SNAKE = (0, 255, 0)           # rgb color for green
 # This is the color for the snake's food.
@@ -48,6 +48,10 @@ def get_direction(previous_direction, event_key):
         return DIRECTION_LEFT
     elif event_key == pygame.K_UP:
         return DIRECTION_UP
+    elif event_key == pygame.K_RIGHT:
+        return DIRECTION_RIGHT
+    elif event_key == pygame.K_DOWN:
+        return DIRECTION_DOWN
     return previous_direction
 
 def create_food_position():
